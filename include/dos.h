b00e8627c008ef63ccfd67767f15ef85503fccdf
Ramadhan Adam <ramadhanzome@gmail.com>
2026-07-11 15:39:56 +0300

    refactor: pe.h holds a DOS_HEADER pointer instead of a raw e_lfanew field

--- include/dos.h
+++ include/dos.h
@@ -2,5 +2,8 @@
 
 #ifndef DOS_H
 #define DOS_H
+#include "pe.h"
+
+int parse_dos_header(PEFile *pe);
 
 #endif // DOS_H
--- include/pe.h
+++ include/pe.h
@@ -3,8 +3,18 @@
 #ifndef PE_H
 #define PE_H
 
+// Minimal DOS header - only the two fields we actually use
 typedef struct {
-    unsigned char *buffer;
-    long size;
+    unsigned short e_magic;      // 2 bytes, offset 0x00 - should be "MZ" (0x5A4D)
+    unsigned char  reserved[58]; // padding, offset 0x02 to 0x3B, unused
+    unsigned int   e_lfanew;     // 4 bytes, offset 0x3C - offset to NT headers
+} DOS_HEADER;
+
+// Holds everything discovered about a PE file as parsing proceeds
+typedef struct {
+    unsigned char *buffer;       // raw file bytes
+    long size;                    // number of bytes in buffer
+    DOS_HEADER *dos_header;       // points into buffer, filled in by dos.c
 } PEFile;
+
 #endif // PE_H
