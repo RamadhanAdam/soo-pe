703f73585b7a8e1e2006e9aeeb9b206e31ad0275
Ramadhan Adam <ramadhanzome@gmail.com>
2026-07-11 14:12:19 +0300

    feat: declare pe_open and pe_open in file.h; ignore generated html

--- include/file.h
+++ include/file.h
@@ -2,5 +2,14 @@
 
 #ifndef FILE_H
 #define FILE_H
+#include "pe.h"
+
+// Opens the file at 'filename' and reads its full contents into pe-> buffer
+// Fills in the pe->size with the number of bytes read
+// Returns 0 on success, non-zero on failure
+int pe_open(const char *filename, PEFile *pe);
+
+// Free memory allocated in pe->bufer by pe_open
+void pe_close(PEFile *pe);
 
 #endif // FILE_H
